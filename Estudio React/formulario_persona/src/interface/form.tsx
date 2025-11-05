50f666e8ad2f519d19eeed3d018c67b5c6268aa0
Bryan-Quispe <rquisper406@gmail.com>
2025-11-04 22:54:24 -0500

    4 NOV 2025

---
+++ Estudio React/formulario_persona/src/interface/form.tsx
@@ -0,0 +1,10 @@
+export interface Persona {
+    name: string;
+    lastName: string;
+    age: number;
+    email: string;
+    phone: string;
+    department: string;
+    academicDegree: string;
+
+}
